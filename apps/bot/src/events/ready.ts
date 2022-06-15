3ab70391b73769c7e24d32debc5959323a27c57f
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 14:31:24 +0500

    Feat(bot): create ready event & set bot presence

---
+++ apps/bot/src/events/ready.ts
@@ -0,0 +1,23 @@
+import { Starship } from "../struct/Client";
+import { logger } from "../logger";
+
+module.exports = async (client: Starship) => {
+    // Log when bot is ready
+    logger.info(`${client.user?.username} is ready for lift-off!`);
+
+    // Set startup presence
+    try {
+        void client.user?.setPresence({
+            status: "online",
+            activities: [
+                {
+                    name: "The Stars!",
+                    type: "WATCHING"
+                }
+            ],
+        })
+    }
+    catch (err: any | unknown) {
+        logger.error(`Failed to set startup presence: ${err.message}`);
+    }
+}
