15b831bca7b358b785551487089bf00d71d066d9
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 22:35:01 +0500

    Feat(bot): create isDiscordHTTPError & isDiscordAPIError functions

--- apps/bot/src/utils.ts
+++ apps/bot/src/utils.ts
@@ -1,4 +1,17 @@
-import { MessageEmbedOptions } from "discord.js";
+import {
+    DiscordAPIError,
+    MessageEmbedOptions
+} from "discord.js";
+
+export const DISCORD_HTTP_ERROR_NAME = "DiscordHTTPError";
+
+export const isDiscordHTTPError = (err: Error | string) => {
+    return typeof err === "object" && err.constructor?.name === DISCORD_HTTP_ERROR_NAME;
+}
+
+export const isDiscordAPIError = (err: Error | string): err is DiscordAPIError => {
+    return err instanceof DiscordAPIError;
+}
 
 export type EmbedWith<T extends keyof MessageEmbedOptions> = MessageEmbedOptions &
     Pick<Required<MessageEmbedOptions>, T>
