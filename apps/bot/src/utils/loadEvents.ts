2b45dc3ea95c1202ced4063b6a577d82564db53c
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 13:11:41 +0500

    Feat(bot): create event handler

---
+++ apps/bot/src/utils/loadEvents.ts
@@ -0,0 +1,18 @@
+import { Starship } from "../struct/Client";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { logger } from "../logger";
+
+// If in prod, use .js else .ts
+const ENV_MODE_FILE_EXT: string = process.env.NODE_ENV === "production" ? ".js" : ".ts";
+
+export const loadEvents = (client:Starship) => {
+    const eventFiles = readdirSync(join(__dirname, "events")).filter(file => file.endsWith(ENV_MODE_FILE_EXT));
+    for (const file of eventFiles) {
+        const event = require(`../events/${file}`);
+        const eventName: string | undefined = file.split(".").shift();
+        client.on(eventName!, event.bind(null, client));
+        delete require.cache[require.resolve(`../events/${file}`)];
+        logger.info(`${eventName} event loaded`);
+    }
+}
