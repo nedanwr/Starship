084a9d86f7d38b936371e0906b745322f8a4529f
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 13:19:32 +0500

    Feat(bot): create command handler

---
+++ apps/bot/src/utils/loadCommands.ts
@@ -0,0 +1,22 @@
+import { Starship } from "../struct/Client";
+import { readdirSync } from "fs";
+import { join } from "path";
+import { logger } from "../logger";
+
+// If in prod, use .js else .ts
+const ENV_MODE_FILE_EXT: string = process.env.NODE_ENV === "production" ? ".js" : ".ts";
+
+export const loadCommands = (client:Starship) => {
+    // Read categories
+    readdirSync(join(__dirname, "commands")).forEach(dir => {
+       // Read commands from categories
+       const commandFiles = readdirSync(join(__dirname, "commands", dir)).filter(file => file.endsWith(ENV_MODE_FILE_EXT));
+       for (const file of commandFiles) {
+           const command = require(`../commands/${dir}/${file}`);
+           if (command.default.trigger) {
+                client.commands.set(command.default.trigger, command.default);
+           }
+           logger.info(`${file.split(".").shift()} command loaded`);
+       }
+    });
+}
