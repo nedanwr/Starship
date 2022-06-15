d8064c658d34f74945fd81385ee83969e843bbe7
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 14:28:35 +0500

    Refactor(bot): move Client config to own file

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,30 +1,8 @@
 import "dotenv/config";
-import { Client, Intents } from "discord.js";
+import { Starship } from "./struct/Client";
 
-const client:Client = new Client({
-    partials: ["USER", "MESSAGE", "CHANNEL", "GUILD_MEMBER", "REACTION"],
-
-    restGlobalRateLimit: 50,
-
-    // Disable mentions by default
-    allowedMentions: {
-        parse: [],
-        users: [],
-        roles: [],
-        repliedUser: false,
-    },
-    intents: [
-        // Privileged
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGE_TYPING,
-
-        // Regular
-        Intents.FLAGS.DIRECT_MESSAGES,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-    ],
+// @ts-ignore
+const client:Starship = new Starship({
+    token: process.env.TOKEN!,
+    prefix: process.env.PREFIX!,
 });
