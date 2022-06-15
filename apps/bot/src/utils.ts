2a067fb52d471db0d62831846ec8b19461bc45f5
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 22:54:20 +0500

    Feat(bot): create isValidSnowflake function

--- apps/bot/src/utils.ts
+++ apps/bot/src/utils.ts
@@ -3,6 +3,19 @@ import {
     MessageEmbedOptions
 } from "discord.js";
 
+// https://discord.com/developers/docs/reference#snowflakes
+export const MIN_SNOWFLAKE: 135169 = 0b000000000000000000000000000000000000000000_00001_00001_000000000001;
+// 0b111111111111111111111111111111111111111111_11111_11111_111111111111 without _ which BigInt doesn't support
+export const MAX_SNOWFLAKE: bigint = BigInt("0b1111111111111111111111111111111111111111111111111111111111111111");
+
+const snowflakePattern: RegExp = /^[1-9]\d+$/;
+export const isValidSnowflake = (str: string) => {
+    if (!str.match(snowflakePattern)) return false;
+    if (parseInt(str, 10) < MIN_SNOWFLAKE) return false;
+    if (BigInt(str) > MAX_SNOWFLAKE) return false;
+    return true;
+}
+
 export const DISCORD_HTTP_ERROR_NAME = "DiscordHTTPError";
 
 export const isDiscordHTTPError = (err: Error | string) => {
@@ -14,4 +27,4 @@ export const isDiscordAPIError = (err: Error | string): err is DiscordAPIError =
 }
 
 export type EmbedWith<T extends keyof MessageEmbedOptions> = MessageEmbedOptions &
-    Pick<Required<MessageEmbedOptions>, T>
+    Pick<Required<MessageEmbedOptions>, T>;
