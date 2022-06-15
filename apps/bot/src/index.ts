96ed3613f1df5007d1e164da52b6a66f816cb3b8
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-06-15 14:29:17 +0500

    Refactor(bot): load events & commands

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -1,8 +1,15 @@
 import "dotenv/config";
 import { Starship } from "./struct/Client";
+import { loadEvents } from "./utils/loadEvents";
+import { loadCommands } from "./utils/loadCommands";
 
-// @ts-ignore
 const client:Starship = new Starship({
     token: process.env.TOKEN!,
     prefix: process.env.PREFIX!,
 });
+
+// Load Events
+loadEvents(client);
+
+// Load Commands
+loadCommands(client);
